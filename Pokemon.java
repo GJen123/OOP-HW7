e1414658f2414f75e3dc0d39cadef13c97ba5958
李霽烝 <GJen@lijizhengde-MacBook-Pro.local>
2016-12-22 13:41:02 +0800

    12/22

---
+++ Pokemon.java
@@ -0,0 +1,29 @@
+
+public abstract class Pokemon {
+	private String name;
+	private PokemonType type;
+	private int cp;
+	
+	public Pokemon(String name, PokemonType type, int cp){
+		this.name = name;
+		this.type = type;
+		this.cp = cp;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public PokemonType getType(){
+		return type;
+	}
+	
+	public void setCp(int cp){
+		this.cp = cp;
+	}
+	public int getCp(){
+		return cp;
+	}
+	
+	public abstract void attack();
+}
